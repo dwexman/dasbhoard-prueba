4a771b225411127133c861dd09b35860e0028376
dwexman <wexmandenise@gmail.com>
2023-09-21 08:58:42 -0300

    funcionando

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
+import { ColorModeContext, useMode } from "./themes";
 import Calendar from "./scenes/calendar";
 
 function App() {
